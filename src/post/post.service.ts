bd484d67c883a88bbe811b3f9e9f832d54150354
ssr0016 <samsonrecluta0016@gmail.com>
2025-09-06 14:00:56 +0800

    inject repository

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Post } from 'src/post/schemas/post.schemas';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+  constructor(@InjectModel(Post.name) private postModel: Model<Post>) {}
+
+  async create() {
+    const createdPost = new this.postModel({ title: 'test', description: 1 });
+
+    return createdPost.save();
+  }
+}
