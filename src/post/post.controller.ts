89304d7d428e7cd58c4e1eadb731e270b06e656a
ssr0016 <samsonrecluta0016@gmail.com>
2025-09-06 20:20:58 +0800

    update all fields (PUT)

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Put } from '@nestjs/common';
 import { TransformDTO } from 'src/interceptors/transform-dto.interceptor';
 import { CreatePostDto } from 'src/post/dtos/create-post.dto';
 import { ResponsePostDto } from 'src/post/dtos/response-post.dto';
+import { UpdatePostDto } from 'src/post/dtos/update-post.dto';
 import { PostService } from 'src/post/post.service';
 
-@Controller('posts') // REST
+@Controller('posts')
 @TransformDTO(ResponsePostDto)
 export class PostController {
   constructor(private readonly postService: PostService) {}
@@ -17,5 +18,15 @@ export class PostController {
   @Get()
   getAll() {
     return this.postService.getAll();
+  }
+
+  @Get('/:id')
+  async getOne(@Param('id') id: string) {
+    return this.postService.getOne(id);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() requestBody: UpdatePostDto) {
+    return this.postService.update(id, requestBody);
   }
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreatePostDto } from 'src/post/dtos/create-post.dto';
+import { UpdatePostDto } from 'src/post/dtos/update-post.dto';
 import { Post } from 'src/post/schemas/post.schemas';
 
 @Injectable()
@@ -17,5 +18,24 @@ export class PostService {
   async getAll() {
     const posts = await this.postModel.find();
     return posts;
+  }
+
+  async getOne(id: string) {
+    const post = await this.postModel.findOne({ _id: id });
+
+    if (!post) {
+      throw new NotFoundException(`Post with id ${id} not found`);
+    }
+
+    return post;
+  }
+
+  async update(id: string, requestBody: UpdatePostDto) {
+    const post = await this.getOne(id);
+
+    post.title = requestBody.title;
+    post.description = requestBody.description;
+
+    return post.save();
   }
 }
